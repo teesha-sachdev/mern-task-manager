68a1fbd135dbd28fc9e6d53f85e8c3c26ea03dac
Teesha Sachdev <teeshasachdev7@gmail.com>
2026-07-17 20:24:26 +0530

    prepare backend for render

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://TeeshaSachdev:Teesha%4017@cluster0.u1pxzij.mongodb.net/?appName=Cluster0");
+        await mongoose.connect("process.env.MONGO_URI");
         console.log("MongoDB Connected");
     } catch (error) {
         console.error("Database Connection Failed:", error.message);
